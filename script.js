cc3e84e2b9708c86b7117a8fa49c18df09fb57d4
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-04-22 18:39:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
 let slider = document.getElementById("myRange");
 let output = document.getElementById("demo");
 let price = document.getElementById("number");
+let price2 = document.getElementById("number-2");
+let x = 0;
+
 output.innerHTML = slider.value; // Display the default slider value
+
+
+// var mq = window.matchMedia( "(max-width: 610px)" );
+// if (mq.matches) {
+    
+// }
+// else {
+//     // window width is greater than 570px
+// }
+
 
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
-  if(output.innerHTML >= 10){
-    price.innerHTML = '$16.00';
-  } 
-  if(output.innerHTML >= 500){
-    price.innerHTML = '$26.00';
-  } 
-  if(output.innerHTML >= 1000){
-    price.innerHTML = '$36.00';
+  let check = document.getElementById("switcher");
+
+  output.innerHTML = this.value ;
+  priceChange();
+
+  if(check.checked == true){
+  checkBox();
   }
+  
+  }
+
+const priceChange = () =>{
+  if(output.innerHTML >= 10){
+    x=1;
+    price.innerHTML = '$8.00';
+  } 
+  if(output.innerHTML >= 50){
+    x=2;
+    price.innerHTML = '$12.00';
+  } 
+  if(output.innerHTML >= 100){
+    x=3;
+    price.innerHTML = '$16.00';
+  }
+  if(output.innerHTML >= 500){
+    x=4;
+    price.innerHTML = '$24.00';
+  }
+  if(output.innerHTML >= 1000){
+    x=5;
+    price.innerHTML = '$36.00';  
+  }
+}
+
+const checkBox = () => {
+  let check = document.getElementById("switcher");
+  if(check.checked == true){
+    switch(x){
+      case 0:
+        price.innerHTML = '$6.00'
+        break;
+      case 1:
+        price.innerHTML = '$6.00'
+        break;
+      case 2:
+        price.innerHTML = '$9.00'
+        break;
+      case 3:
+        price.innerHTML = '$12.00'
+        break;
+      case 4:
+        price.innerHTML = '$18.00'
+        break;
+      case 5:
+        price.innerHTML = '$28.00'
+        break;
+    }
+    }else{
+      priceChange()
+    }   
 }
